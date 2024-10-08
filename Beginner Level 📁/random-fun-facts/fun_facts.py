2cebc946b8be1d7d852d6096e03c4078f6788083
AhmedFaizanDev <info2faizanahmed@gmail.com>
2024-10-08 17:30:37 +0530

    Added Random Fun Fact Generator task

---
+++ Beginner Level 📁/random-fun-facts/fun_facts.py
@@ -0,0 +1,13 @@
+import random
+
+# List of fun facts
+fun_facts = [
+    "Did you know? Honey never spoils!",
+    "Octopuses have three hearts!",
+    "Bananas are berries, but strawberries aren't!",
+    "There are more stars in the universe than grains of sand on Earth!",
+    "A day on Venus is longer than a year on Venus!"
+]
+
+# Randomly select and print a fun fact
+print(random.choice(fun_facts))
---
+++ Beginner Level 📁/random-fun-facts/question.md
@@ -0,0 +1,11 @@
+# Random Fun Fact Generator Task
+
+## Task Description:
+Write a Python program that displays a random fun fact each time it's executed.
+
+### Requirements:
+- Use a list of at least 5 fun facts.
+- Randomly select and display one fact from the list.
+- The program should run in Python.
+
+### Example Output:
